3b4b2afc1ea3260d77baee5cc8f5a209504320ab
Peter Liu <onhout@gmail.com>
2020-03-03 23:31:03 +0000

    added received status

--- asc_therapeutics_customize/models/models.py
+++ asc_therapeutics_customize/models/models.py
@@ -2,79 +2,92 @@
 
 from odoo import models, fields, api
 
-# class asc_therapeutics_customize(models.Model):
-#     _name = 'asc_therapeutics_customize.asc_therapeutics_customize'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
-
 #Extending the purchase order model, uses Catalog Number by default
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
-
     @api.onchange('product_id')
     def product_id_change(self):
         if (self.product_id.x_catalog_number):
             self.name += " Catalog No.: " + self.product_id.x_catalog_number
 
+#Add Received Status to Purchase Orders
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+    state = fields.Selection([
+        ('draft', 'RFQ'),
+        ('sent', 'RFQ Sent'),
+        ('to approve', 'To Approve'),
+        ('purchase', 'Purchase Order'),
+        ('done', 'Locked'),
+        ('cancel', 'Cancelled'),
+        ('received', 'Received')
+    ], string='Status', readonly=True, index=True, copy=False, default='draft', track_visibility='onchange')
+
+#Add interaction for setting received status after the transfer is set to done
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    def read(self, fields=None, load='_classic_read'):
+        res = super(StockPicking, self).read(fields=fields, load=load)
+        
+        for i in res:
+            for po in self:
+                if po.id == i['id'] and 'state' in i and i['state'] == 'done':
+                    po.purchase_id.write({'state': 'received'})
+
+        return res
+
 
 # General Override Zoom Emailing
-class CalendarEvent(models.Model):
-    _inherit = 'calendar.event'
+# class CalendarEvent(models.Model):
+#     _inherit = 'calendar.event'
     
-    def read(self, fields=None, load='_classic_read'):
-        res = super(CalendarEvent, self).read(fields=fields, load=load)
+#     def read(self, fields=None, load='_classic_read'):
+#         res = super(CalendarEvent, self).read(fields=fields, load=load)
         
-        for i in self:
-            print (i.name)    
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        return res
+#         for i in self:
+#             print (i.name)    
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         return res
 
-    @api.model
-    def create(self, values):
-        res = super(CalendarEvent, self).create(values)
+#     @api.model
+#     def create(self, values):
+#         res = super(CalendarEvent, self).create(values)
         
-        print(res)
+#         print(res)
     
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        return res
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         return res
     
     
-    def write(self, values):
-        res = super(CalendarEvent, self).write(values)
+#     def write(self, values):
+#         res = super(CalendarEvent, self).write(values)
         
-        print(values)
+#         print(values)
     
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
-        return res
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         print("AIOSJDOAISJDIOAJSDIOJASODJAOISDJOAISJDIOAJSDIOJASD")
+#         return res
